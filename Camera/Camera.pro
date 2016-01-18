12a1adaf93aab71e80733c27b0303c4a1f71ef89
bmichell <benjamin.michelland@institutoptique.fr>
2016-01-18 12:03:33 +0100

    Code OpenCV

---
+++ Camera/Camera.pro
@@ -0,0 +1,19 @@
+TEMPLATE = app
+CONFIG += console
+CONFIG -= app_bundle
+CONFIG -= qt
+INCLUDEPATH += /usr/include/opencv2
+LIBS += -lopencv_core
+LIBS += -lopencv_imgproc
+LIBS += -lopencv_highgui
+LIBS += -lopencv_ml
+LIBS += -lopencv_video
+LIBS += -lopencv_features2d
+LIBS += -lopencv_calib3d
+LIBS += -lopencv_objdetect
+LIBS += -lopencv_contrib
+LIBS += -lopencv_legacy
+LIBS += -lopencv_flann
+
+SOURCES += main.cpp
+
---
+++ Camera/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <video/video.hpp>
+#include <opencv.hpp>
+
+using namespace cv;
+
+int main(int, char**)
+{
+    VideoCapture cap(0); // open the default camera
+    if(!cap.isOpened())  // check if we succeeded
+        return -1;
+
+    Mat edges;
+    namedWindow("edges",1);
+    for(;;)
+    {
+        Mat frame;
+        cap >> frame; // get a new frame from camera
+        cvtColor(frame, edges, CV_BGR2GRAY);
+        imshow("edges", frame);
+        if(waitKey(30) >= 0) break;
+    }
+    // the camera will be deinitialized automatically in VideoCapture destructor
+    return 0;
+}
