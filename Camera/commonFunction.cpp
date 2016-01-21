b5311fc956e83d3e6346e3d452db2a32d705afab
Benjamin <benjamin.michelland@institutoptique.fr>
2016-01-21 17:02:03 +0100

    Vitesse

--- Camera/commonFunction.cpp
+++ Camera/commonFunction.cpp
@@ -9,6 +9,44 @@ std::vector<int> threeNearest(Rect p, std::vector<std::vector<Rect> > f)
         std::cout << "il n'y a pas assez de points" << std::endl;
     }
 
+<<<<<<< HEAD
+    double dot1 = std::numeric_limits<double>::max();
+    double dot2 = std::numeric_limits<double>::max();
+    double dot3 = std::numeric_limits<double>::max();
+    std::vector<int> out(3); out[0]=-1; out[1]=-1; out[2]=-1;
+    Point_<int> pp = p.tl() + Point_<int>(p.width/2,p.height/2);
+    for(unsigned int i = 0; i < f[0].size(); i++)
+    {
+        Point_<int> fp = f[0][i].tl() + Point_<int>(f[0][i].width/2,f[0][i].height/2);
+        double dot = (pp-fp).dot(pp-fp);
+        if(dot1>dot && dot <= 25000.0)
+        {
+            dot1=dot;
+            out[0]=i;
+        }
+    }
+    for(unsigned int i = 0; i < f[1].size(); i++)
+    {
+        Point_<int> fp = f[1][i].tl() + Point_<int>(f[1][i].width/2,f[1][i].height/2);
+        double dot = (pp-fp).dot(pp-fp);
+        if(dot2>dot && dot <= 25000.0)
+        {
+            dot2=dot;
+            out[1]=i;
+        }
+    }
+    for(unsigned int i = 0; i < f[2].size(); i++)
+    {
+        Point_<int> fp = f[2][i].tl() + Point_<int>(f[2][i].width/2,f[2][i].height/2);
+        double dot = (pp-fp).dot(pp-fp);
+        if(dot3>dot && dot <= 25000.0)
+        {
+            dot3=dot;
+            out[2]=i;
+        }
+    }
+
+=======
     int dot1 = p.tl().dot(f[0][0].tl());
     int dot2 = p.tl().dot(f[1][0].tl());
     int dot3 = p.tl().dot(f[2][0].tl());
@@ -37,5 +75,6 @@ std::vector<int> threeNearest(Rect p, std::vector<std::vector<Rect> > f)
             out[2]=i;
         }
     }
+>>>>>>> b627d60e3b8e59da6299997913fa8606a8c7345e
     return out;
 }
--- Camera/main.cpp
+++ Camera/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <iomanip>
 #include <video/video.hpp>
 #include <opencv.hpp>
 #include "commonFunction.h"
 #include "fragile.h"
 
 using namespace cv;
-
+bool init = false;
 int main(int argc, char** argv)
 {
     Fragile f;
@@ -23,12 +24,30 @@ int main(int argc, char** argv)
     std::vector<std::vector<Rect> > found_filtered(3,std::vector<Rect>(0));
     int k = 0;
     Mat frame;
+<<<<<<< HEAD
+    Mat rW;
+    Rect save(Point_<int>(0,0), Point_<int>(0,0));
+    Point2d prevPos;
+    std::vector<Point2d> src(4);
+    src[0] = Point2d(263,281); src[1] = Point2d(7,265); src[2] = Point2d(357,147); src[3] = Point2d(161,147);
+    std::vector<Point2d> dest(4);
+    dest[0] = Point2d(3,0); dest[1] = Point2d(0,0); dest[2] = Point2d(3,4.5); dest[3] = Point2d(0,3);
+    Mat H;
+    H = findHomography(src,dest,H);
+=======
     Rect save(Point_<int>(0,0),Point_<int>(0,0));
+>>>>>>> b627d60e3b8e59da6299997913fa8606a8c7345e
     for(;;)
     {
         foundsave = found_filtered[k%3];
         found_filtered[k%3] = std::vector<Rect>(0);
         cap >> frame; // get a new frame from camera
+        if(k==0)
+        {
+            rW = frame;
+            warpPerspective(frame,rW,H,Size(1000,1000));
+            imwrite("/home/benjamin.michelland/calibrated.png",rW);
+        }
         cvtColor(frame, mono_img, CV_BGR2GRAY);
         hog.detectMultiScale(mono_img, found,-0.5);
         size_t i, j;
@@ -51,6 +70,35 @@ int main(int argc, char** argv)
             {
                 std::vector<int> list = threeNearest(save,found_filtered);
                 for(unsigned i = 0; i < found_filtered[0].size(); i++) {
+<<<<<<< HEAD
+                    if(list[0] != -1 && list[1] != -1 && list[2] !=-1)
+                    {
+                        Rect r1 = found_filtered[0][list[0]];
+                        Rect r2 = found_filtered[1][list[1]];
+                        Rect r3 = found_filtered[2][list[2]];
+                        Point_<int> c1 = r1.tl() + Point_<int>(r1.width/2,r1.height-10);
+                        c1 = c1 + r2.tl() + Point_<int>(r2.width/2,r2.height-10);
+                        c1 = c1 + r3.tl() + Point_<int>(r3.width/2,r3.height-10);
+                        c1 = Point_<int>(c1.x/3.0,c1.y/3.0);
+                        rectangle(frame, c1, c1, Scalar(0,255,0), 2);
+                        save = Rect(c1,c1);
+                    }
+                }
+                float speed = 0;
+                if(k%25==0)
+                {
+                    std::vector<Point2d> pix(1);
+                    pix[0] = save.tl();
+                    std::vector<Point2d> pos(1);
+                    perspectiveTransform(pix,pos,H);
+                if(init)
+                {
+                    speed = sqrt((pos[0]-prevPos).dot(pos[0]-prevPos));
+                    std::cout << std::setprecision(3) << speed << " m/s" << std::endl;
+                }
+                prevPos=pos[0];
+                init = true;
+=======
                     Rect r1 = found_filtered[0][list[0]];
                     Rect r2 = found_filtered[1][list[1]];
                     Rect r3 = found_filtered[2][list[2]];
@@ -63,6 +111,7 @@ int main(int argc, char** argv)
                     c1 = Point_<int>(c1.x/3.0,c1.y/3.0); c2 = Point_<int>(c2.x/3.0,c2.y/3.0);
                     rectangle(frame, c1, c2, Scalar(0,255,0), 2);
                     save = Rect(c1,c2);
+>>>>>>> b627d60e3b8e59da6299997913fa8606a8c7345e
                 }
             }
         }
